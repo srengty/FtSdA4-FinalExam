dee20702e8d3110817d35e6970366504a82d5577
Noun Sunheng <nounSunheng@gmail.com>
2024-02-16 14:51:50 +0700

    Updated search ingedients

--- CookBook/Models/Recipe.cs
+++ CookBook/Models/Recipe.cs
@@ -12,5 +12,12 @@ namespace CookBook.Models
         public List<string> Images { get; set; } = [];
         public List<string> Ingredients { get; set; } = []; //search Ingredients
         public string Instructions { get; set; } = "";
+        public bool ContainsIngredients(IEnumerable<string> ingredients)
+        {
+            var lowercaseIngredients = Ingredients.Select(i => i.ToLower());
+            var lowercaseInputIngredients = ingredients.Select(i => i.ToLower());
+
+            return lowercaseInputIngredients.All(i => lowercaseIngredients.Contains(i));
+        }
     }
 }
--- CookBook/ViewModels/MainViewModel.cs
+++ CookBook/ViewModels/MainViewModel.cs
@@ -5,8 +5,10 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using CommunityToolkit.Mvvm;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CookBook.Models;
 
 namespace CookBook.ViewModels
@@ -27,6 +29,54 @@ namespace CookBook.ViewModels
         {
             cuisines = new ObservableCollection<Cuisine>(cuisineList.Cuisines);
             SelectedCuisine = cuisines[0];
+        }
+        private ObservableCollection<Recipe> filteredRecipes;
+        private string searchIngredients = "";
+        private string searchResultMessage = "";
+
+        public string SearchIngredients
+        {
+            get => searchIngredients;
+            set => SetProperty(ref searchIngredients, value);
+        }
+
+        public string SearchResultMessage
+        {
+            get => searchResultMessage;
+            set => SetProperty(ref searchResultMessage, value);
+        }
+
+        public ObservableCollection<Recipe> FilteredRecipes
+        {
+            get => filteredRecipes;
+            set => SetProperty(ref filteredRecipes, value);
+        }
+
+        public ICommand SearchIngredientsCommand => new RelayCommand(SearchByIngredients);
+
+        private void SearchByIngredients()
+        {
+            if (string.IsNullOrWhiteSpace(SearchIngredients))
+            {
+                FilteredRecipes = new ObservableCollection<Recipe>();
+                return;
+            }
+
+            var inputIngredients = SearchIngredients.Split(',').Select(i => i.Trim().ToLower());
+            var matchingRecipes = Cuisines.SelectMany(c => c.Recipes)
+                                          .Where(r => r.ContainsIngredients(inputIngredients))
+                                          .ToList();
+
+            if (matchingRecipes.Any())
+            {
+                FilteredRecipes = new ObservableCollection<Recipe>(matchingRecipes);
+                SearchResultMessage = "";
+            }
+            else
+            {
+                FilteredRecipes = new ObservableCollection<Recipe>();
+                SearchResultMessage = "Ingredients not found.";
+            }
         }
     }
 }
